74a4bdaae47a98acc78b4fcd049c0fc53813c499
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2021-10-31 16:56:59 +0100

    add pagination config validation (#22)

--- test/fryse/config_test.exs
+++ test/fryse/config_test.exs
@@ -54,6 +54,78 @@ defmodule Fryse.ConfigTest do
     end
   end
 
+  describe "validate :paginations key" do
+    test "the key must be not nil" do
+      assert %MissingConfigValue{key: :paginations} = Config.validate_key(:paginations, nil)
+      assert %MissingConfigValue{key: :paginations} = Config.validate_key(:paginations, "")
+      assert :ok = Config.validate_key(:paginations, [])
+    end
+
+    test "the key must be a list" do
+      assert :ok = Config.validate_key(:paginations, [])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, 1)
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, true)
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, "hello")
+    end
+
+    test "the items must contain all keys and they must have the right value type" do
+      valid_item = %{
+        name: "posts",
+        from_folder: "posts",
+        size: 1,
+        sort: "order|desc",
+        path: "/posts/:page",
+        list_template: "/posts/index.html.eex",
+        first_page: "index.html.eex",
+      }
+
+      assert :ok = Config.validate_key(:paginations, [valid_item])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [Map.delete(valid_item, :name)])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [Map.delete(valid_item, :from_folder)])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [Map.delete(valid_item, :size)])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [Map.delete(valid_item, :sort)])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [Map.delete(valid_item, :path)])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [Map.delete(valid_item, :list_template)])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [Map.delete(valid_item, :first_page)])
+
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | name: nil}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | name: ""}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | name: 0}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | name: true}])
+
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | from_folder: nil}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | from_folder: ""}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | from_folder: 0}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | from_folder: true}])
+
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | size: nil}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | size: ""}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | size: 0}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | size: -1}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | size: true}])
+
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | sort: nil}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | sort: ""}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | sort: 0}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | sort: true}])
+
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | path: nil}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | path: ""}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | path: 0}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | path: true}])
+
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | list_template: nil}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | list_template: ""}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | list_template: 0}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | list_template: true}])
+
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | first_page: nil}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | first_page: ""}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | first_page: 0}])
+      assert %InvalidConfigValue{key: :paginations} = Config.validate_key(:paginations, [%{valid_item | first_page: true}])
+    end
+  end
+
   describe "validate :clean_urls key" do
     test "the key must be a boolean" do
       assert :ok = Config.validate_key(:clean_urls, true)
